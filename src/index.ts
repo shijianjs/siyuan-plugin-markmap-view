078036a2cf51aa679118a3cd4b0c1f09b5f19503
shijianjs <shijianjs@hotmail.com>
2026-03-02 20:33:03 +0800

    fix #12 支持在侧边窗口打开
    feat(core): 添加侧边栏停靠功能并重构渲染逻辑
    
    - 添加了新的国际化字段 syncMdContent 用于同步Markdown内容
    - 格式化了SCSS样式文件中的CSS类定义
    - 从siyuan库导入adaptHotkey函数用于热键适配
    - 添加了DOCK_TYPE常量定义停靠类型
    - 将settingUtils属性改为public访问权限
    - 添加了完整的侧边栏停靠面板功能，包括初始化、更新、销毁生命周期方法
    - 实现了独立的MarkmapRender类来管理思维导图渲染逻辑
    - 在停靠面板中添加了刷新按钮用于同步文档内容
    - 实现了暗色主题切换功能
    - 更新了包版本号至1.6.0
    - 优化了对话框和停靠面板的渲染性能
    - 移除了重复的私有方法定义，统一为公共属性访问

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import {Dialog, getAllEditor, IProtyle, Plugin, showMessage} from "siyuan";
+import {adaptHotkey, Dialog, getAllEditor, IProtyle, Plugin, showMessage} from "siyuan";
 import {deriveOptions, Markmap} from 'markmap-view';
 import {Toolbar} from 'markmap-toolbar';
 import {Transformer} from 'markmap-lib';
@@ -47,9 +47,11 @@ enum MarkdownSourceMode {
     exportMdContent = "exportMdContent"
 }
 
+export const DOCK_TYPE = "markmap_dock";
+
 export default class SiYuanMarkmapViewPlugin extends Plugin {
 
-    private settingUtils: SettingUtils;
+    settingUtils: SettingUtils;
     typedI18n: typeof zh_CN
 
     async onload() {
@@ -211,34 +213,78 @@ export default class SiYuanMarkmapViewPlugin extends Plugin {
             position: "right",             // 可选：left / right
             callback: () => this.openMarkmapDialog(),
         });
+        let dockMarkmapRender = new MarkmapRender({plugin:this});
+        this.addDock({
+            config: {
+                position: "RightTop",
+                size: {width: 400, height: 0},
+                icon: ICON_NAME,
+                title: this.typedI18n.actionName,
+            },
+            data: {
+            },
+            type: DOCK_TYPE,
+            resize() {
+                console.log(DOCK_TYPE + " resize");
+            },
+            update() {
+                console.log(DOCK_TYPE + " update");
+            },
+            init: (dock) => {
+                console.log("init dock:", DOCK_TYPE)
+                dock.element.innerHTML = `<div class="fn__flex-1 fn__flex-column">
+                    <div class="block__icons">
+                        <div class="block__logo">
+                            <svg class="block__logoicon"><use xlink:href="#${ICON_NAME}"></use></svg>
+                            ${this.typedI18n.actionName}
+                        </div>
+
+                        <span class="fn__flex-1 fn__space"></span>
+                        <span data-type="refresh" class="refresh_btn block__icon b3-tooltips b3-tooltips__sw" aria-label="${this.typedI18n.syncMdContent}">
+                            <svg class=""><use xlink:href="#iconRefresh"></use></svg>
+                        </span>
+                        <span data-type="min" class="block__icon b3-tooltips b3-tooltips__sw" aria-label="Min ${adaptHotkey("⌘W")}">
+                            <svg class="block__logoicon"><use xlink:href="#iconMin"></use></svg>
+                        </span>
+                    </div>
+                    <div class="fn__flex-1 fn__flex-column markmap-view-container">
+                        <svg class="markmap-view-svg fn__flex-1" />
+                    </div>
+                    </div>`;
+                let refreshBtn:HTMLElement = <HTMLElement>dock.element.getElementsByClassName("refresh_btn")[0];
+                // let dockMarkmapRender = new MarkmapRender({plugin:this,contextElement:dock.element as HTMLElement});
+                dockMarkmapRender.contextElement = dock.element as HTMLElement
+                let refreshMarkdown = async () => {
+                    console.log("refresh");
+                    dockMarkmapRender.mm?.destroy()
+                    await dockMarkmapRender.init()
+                    await dockMarkmapRender.renderMarkmap()
+                };
+                refreshBtn.onclick = refreshMarkdown
+                setTimeout(()=>{
+                    refreshMarkdown()
+                },1000)
+                // }
+            },
+            destroy() {
+                console.log("destroy dock:", DOCK_TYPE);
+                dockMarkmapRender.mm?.destroy()
+            }
+        });
 
     }
 
     async openMarkmapDialog() {
-        this.initDarkTheme();
 
-        let protyle: IProtyle = this.getEditor().protyle;
-        const docId = protyle.block.rootID;
-        const docInfoResp = await client.getDocInfo({id: docId})
-        let title: string = docInfoResp.data.name;
-
-        let mode = this.settingUtils.get(markdownSourceModeName);
-        let markdown:string;
-        if (mode === MarkdownSourceMode.getDoc) {
-            const docResp = await client.getDoc({id: docId})
-            let docHtmlContent = docResp.data.content;
-            markdown = protyle.lute.BlockDOM2StdMd(docHtmlContent)
-        } else {
-            const mdResp = await client.exportMdContent({id: docId})
-            markdown = truncateAtFootnote1(mdResp.data.content);
-            markdown = removeUntilFirstH1(markdown);
-            console.log("markdown", markdown)
-        }
+        let markmapRender = new MarkmapRender({
+            plugin: this,
+        });
+        await markmapRender.init()
 
         // console.log("markdown", markdown)
         // console.log("点击了生成思维导图", this.name, docId, this.getEditor().protyle, doc, markdown);
         const dialog = new Dialog({
-            title: `Markmap - ${title}`,
+            title: `Markmap - ${markmapRender.title}`,
             content: `
                         <div class="markmap-view-container">
                             <svg class="markmap-view-svg" />
@@ -248,46 +294,15 @@ export default class SiYuanMarkmapViewPlugin extends Plugin {
             height: '95vh',
             destroyCallback: () => {
                 console.log("destroyCallback 关闭了");
-                mm?.destroy()
+                markmapRender.mm?.destroy()
             }
         });
-        const containerDiv: HTMLElement = dialog.element.querySelector(".markmap-view-container");
-        const markmapSvg = dialog.element.querySelector(".markmap-view-svg");
-        let transformResult = transformer.transform(markdown);
-        // console.log("transformResult", transformResult)
-        const mm = Markmap.create(markmapSvg, deriveOptions({
-            colorFreezeLevel: this.settingUtils.get(colorFreezeLevelName),
-            duration: this.settingUtils.get(durationName),
-            maxWidth: this.maxWidth,
-            initialExpandLevel: this.settingUtils.get(initialExpandLevelName),
-            zoom: this.settingUtils.get(zoomName),
-            pan: this.settingUtils.get(panName),
-            spacingHorizontal: this.settingUtils.get(spacingHorizontalName),
-            spacingVertical: this.settingUtils.get(spacingVerticalName),
-            
-            // 不太好设置，就用默认的了，默认也是递减的
-            // lineWidth: (node)=>{
-            //     let depth = node.state.depth;
-            //     let w = 4- depth;
-            //     w = Math.max(w, 1);
-            //     return w;
-            // },
-        }));
-        transformResult.root.content = title
-        await mm.setData(transformResult.root, deriveOptions(transformResult.frontmatter?.markmap ?? {}));
-        await mm.fit();
-        let toolbar = Toolbar.create(mm);
-        containerDiv.append(toolbar.el);
-        // toolbar.el.classList.add('button-panel');
-        let downloadSvgBtn = this.createDownloadBtn(mm, title);
-        toolbar.el.appendChild(downloadSvgBtn)
-        // 自动聚焦，意义不大
-        // setTimeout(() => {
-        //     let dialogContainer= containerDiv
-        //     dialogContainer.tabIndex = -1; // 使其可聚焦
-        //     dialogContainer.focus({ preventScroll: true });
-        // }, 100);
+        markmapRender.contextElement = dialog.element;
+        // const mm = await this.renderMarkmap(contextElement, markdown, title);
+
+        await markmapRender.renderMarkmap()
     }
+
 
     get colorFreezeLevel() {
         return this.settingUtils.get(colorFreezeLevelName);
@@ -317,9 +332,131 @@ export default class SiYuanMarkmapViewPlugin extends Plugin {
         return this.settingUtils.get(panName);
     }
 
+
+    async onunload() {
+        console.log("onunload");
+        // this.cleanDarkModeClass();
+    }
+
+    uninstall() {
+        console.log("uninstall");
+    }
+
+
+    getEditor() {
+        const editors = getAllEditor();
+        if (editors.length === 0) {
+            showMessage(`请先打开文档`, 6000, "error");
+            return;
+        }
+        let protyle = getProtyle();
+        return {protyle};
+    }
+
+
+    initDarkTheme() {
+        let html = document.documentElement;
+        if (html.getAttribute('data-theme-mode') === "dark") {
+            html.classList.add(darkModeClassName);
+        } else {
+            this.cleanDarkModeClass();
+        }
+    }
+
+    cleanDarkModeClass() {
+        document.documentElement.classList.remove(darkModeClassName);
+    }
+}
+
+class MarkmapRender {
+    plugin: SiYuanMarkmapViewPlugin
+    settingUtils: SettingUtils;
+
+    contextElement: HTMLElement
+
+
+    protyle: IProtyle
+    docId: string;
+    markdown: string
+    title: string
+
+    mm: Markmap
+
+    constructor(props?: Partial<MarkmapRender>) {
+        Object.assign(this, props);
+    }
+
+    async init() {
+        this.plugin.initDarkTheme();
+        this.settingUtils = this.plugin.settingUtils
+
+        let protyle: IProtyle = this.plugin.getEditor().protyle;
+        this.protyle = protyle;
+        const docId = protyle.block.rootID;
+        this.docId = docId;
+        const docInfoResp = await client.getDocInfo({id: docId})
+        let title: string = docInfoResp.data.name;
+        this.title = title;
+
+        let mode = this.settingUtils.get(markdownSourceModeName);
+        let markdown: string;
+        if (mode === MarkdownSourceMode.getDoc) {
+            const docResp = await client.getDoc({id: docId})
+            let docHtmlContent = docResp.data.content;
+            markdown = protyle.lute.BlockDOM2StdMd(docHtmlContent)
+        } else {
+            const mdResp = await client.exportMdContent({id: docId})
+            markdown = truncateAtFootnote1(mdResp.data.content);
+            markdown = removeUntilFirstH1(markdown);
+            console.log("markdown", markdown)
+        }
+        this.markdown = markdown;
+    }
+
+    async renderMarkmap() {
+        const containerDiv: HTMLElement = this.contextElement.querySelector(".markmap-view-container");
+        const markmapSvg = this.contextElement.querySelector(".markmap-view-svg");
+        let transformResult = transformer.transform(this.markdown);
+        // console.log("transformResult", transformResult)
+        const mm: Markmap = Markmap.create(markmapSvg, deriveOptions({
+            colorFreezeLevel: this.settingUtils.get(colorFreezeLevelName),
+            duration: this.settingUtils.get(durationName),
+            maxWidth: this.plugin.maxWidth,
+            initialExpandLevel: this.settingUtils.get(initialExpandLevelName),
+            zoom: this.settingUtils.get(zoomName),
+            pan: this.settingUtils.get(panName),
+            spacingHorizontal: this.settingUtils.get(spacingHorizontalName),
+            spacingVertical: this.settingUtils.get(spacingVerticalName),
+
+            // 不太好设置，就用默认的了，默认也是递减的
+            // lineWidth: (node)=>{
+            //     let depth = node.state.depth;
+            //     let w = 4- depth;
+            //     w = Math.max(w, 1);
+            //     return w;
+            // },
+        }));
+        transformResult.root.content = this.title
+        await mm.setData(transformResult.root, deriveOptions(transformResult.frontmatter?.markmap ?? {}));
+        await mm.fit();
+        let toolbar = Toolbar.create(mm);
+        containerDiv.append(toolbar.el);
+        // toolbar.el.classList.add('button-panel');
+        let downloadSvgBtn = this.createDownloadBtn(mm, this.title);
+        toolbar.el.appendChild(downloadSvgBtn)
+        // 自动聚焦，意义不大
+        // setTimeout(() => {
+        //     let dialogContainer= containerDiv
+        //     dialogContainer.tabIndex = -1; // 使其可聚焦
+        //     dialogContainer.focus({ preventScroll: true });
+        // }, 100);
+        this.mm = mm;
+        return mm;
+    }
+
     private createDownloadBtn(mm: Markmap, title: string) {
         let downloadSvgBtn = document.createElement("div");
-        downloadSvgBtn.title = this.typedI18n.exportSvgImage
+        downloadSvgBtn.title = this.plugin.typedI18n.exportSvgImage
         downloadSvgBtn.innerHTML = '🖼️'
         downloadSvgBtn.classList.add('mm-toolbar-item')
         downloadSvgBtn.addEventListener("click", () => {
@@ -335,36 +472,4 @@ export default class SiYuanMarkmapViewPlugin extends Plugin {
         return downloadSvgBtn;
     }
 
-    async onunload() {
-        console.log("onunload");
-        this.cleanDarkModeClass();
-    }
-
-    uninstall() {
-        console.log("uninstall");
-    }
-
-
-    private getEditor() {
-        const editors = getAllEditor();
-        if (editors.length === 0) {
-            showMessage(`请先打开文档`, 6000, "error");
-            return;
-        }
-        let protyle = getProtyle();
-        return {protyle};
-    }
-
-    private initDarkTheme() {
-        let html = document.documentElement;
-        if (html.getAttribute('data-theme-mode') === "dark") {
-            html.classList.add(darkModeClassName);
-        } else {
-            this.cleanDarkModeClass();
-        }
-    }
-
-    private cleanDarkModeClass() {
-        document.documentElement.classList.remove(darkModeClassName);
-    }
 }
